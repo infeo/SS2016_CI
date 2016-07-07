530d8c106c151d328177cee1afa4df9833bfb3ee
infeo <s6aaschr@uni-bonn.de>
2016-07-07 14:34:15 +0200

    ALLGEMEIN Letzte Anpassung der Klassen vor Abgabe

--- Java/NeuralNetwork/src/evolutionaryAlgorithm/Evo.java
+++ Java/NeuralNetwork/src/evolutionaryAlgorithm/Evo.java
@@ -14,151 +14,160 @@ public class Evo {
 	private int pop_size;
 	private int genome_length;
 	private int numElite;
-	private double [] fitness;
-	private double [][] population;
-	private boolean [] elite;
+	private double[] fitness;
+	private double[][] population;
+	private boolean[] elite;
 	private double mutate_probability;
 	private int numCrossover;
-	private Measure<double []> measure;
-	//count of elite, which are directly transfered to the next generation
+	private Measure<double[]> measure;
+	// count of elite, which are directly transfered to the next generation
 	private PriorityQueue<FastEntry> ranking;
-	
-	public Evo(int genome_length, int pop_size, int numElite, double mutate_probability, int numCrossover, Measure<double[]> measure){
-		if(pop_size<1) throw new IllegalArgumentException("Die Populationsgr��e muss positiv sein!");
-//		if(mutator == null) throw new IllegalArgumentException("Der Mutationsklasse darf nicht null sein!");
-		if(genome_length<2) throw new IllegalArgumentException("Die Anzahl der St�tzstellen muss mindestens 2 sein!");
-		if(mutate_probability>=1 && mutate_probability<=0) throw new IllegalArgumentException();
-		this.pop_size=pop_size;
+
+	public Evo(int genome_length, int pop_size, int numElite, double mutate_probability, int numCrossover,
+			Measure<double[]> measure) {
+		if (pop_size < 1)
+			throw new IllegalArgumentException("Die Populationsgr��e muss positiv sein!");
+		// if(mutator == null) throw new IllegalArgumentException("Der
+		// Mutationsklasse darf nicht null sein!");
+		if (genome_length < 2)
+			throw new IllegalArgumentException("Die Anzahl der St�tzstellen muss mindestens 2 sein!");
+		if (mutate_probability >= 1 && mutate_probability <= 0)
+			throw new IllegalArgumentException();
+		this.pop_size = pop_size;
 		this.mutate_probability = mutate_probability;
-		this.genome_length=genome_length;
-		this.numElite=numElite;
-		this.numCrossover=numCrossover;
+		this.genome_length = genome_length;
+		this.numElite = numElite;
+		this.numCrossover = numCrossover;
 		this.elite = new boolean[pop_size];
-		//init the measurearray
-		this.fitness = new double [pop_size];
+		// init the measurearray
+		this.fitness = new double[pop_size];
 		Arrays.fill(fitness, Double.NaN);
-		this.measure=measure;
-		
-		//init the population
+		this.measure = measure;
+
+		// init the population
 		this.population = new double[pop_size][genome_length];
-		
-		for(int i =0;i<pop_size;i++){
-			double [] individual = new double[genome_length];
-			for(int j =0; j<genome_length; j++){
-				individual[j]=ThreadLocalRandom.current().nextDouble();
+
+		for (int i = 0; i < pop_size; i++) {
+			double[] individual = new double[genome_length];
+			for (int j = 0; j < genome_length; j++) {
+				individual[j] = ThreadLocalRandom.current().nextDouble();
 			}
-			population[i]=individual;
+			population[i] = individual;
 		}
-		
-		//init the priorityqueue to get quick acess to the elite
-		ranking = new PriorityQueue<FastEntry>(pop_size,Map.Entry.comparingByKey());
+
+		// init the priorityqueue to get quick acess to the elite
+		ranking = new PriorityQueue<FastEntry>(pop_size, Map.Entry.comparingByKey());
 	}
-	
-	public void adoptAdaptDevelop(int iterations){
-		for(int i =1; i<iterations;i++){
-			//1. eval
+
+	public void adoptAdaptDevelop(int iterations) {
+		for (int i = 1; i < iterations; i++) {
+			// 1. eval
 			eval();
-			//2.select & 3.reproduct
+			// 2.select & 3.reproduct
 			select();
-			//4.mutate
+			// 4.mutate
 			mutateNCross();
 		}
 	}
-	
-	public double [] getBest(){
+
+	public double[] getBest() {
 		eval();
-		double max =fitness[0];
-		int max_index=0;
-		for(int i=0;i<pop_size;i++)
-			if(max < fitness[i]){
-				max=fitness[i];
-				max_index=i;
+		double max = fitness[0];
+		int max_index = 0;
+		for (int i = 0; i < pop_size; i++)
+			if (max < fitness[i]) {
+				max = fitness[i];
+				max_index = i;
 			}
 		return population[max_index];
 	}
-	
-	private void eval(){
-		for(int i =0; i<pop_size;i++){
-			fitness[i]=1/measure.measure(population[i]);
-			ranking.offer(new FastEntry(fitness[i],i));
+
+	private void eval() {
+		for (int i = 0; i < pop_size; i++) {
+			fitness[i] = 1 / measure.measure(population[i]);
+			ranking.offer(new FastEntry(fitness[i], i));
 		}
 	}
-	
-	private void select(){
+
+	private void select() {
 		Arrays.fill(elite, true);
-		double [] []new_pop = new double [pop_size][genome_length];
+		double[][] new_pop = new double[pop_size][genome_length];
 		Random rand = new Random();
-		double rest = pop_size-numElite;
-		double [] sectors = new double [(int)rest];
-		int [] indexToSector= new int [(int) rest];
-		
-		/* Computes the upper bounds of the sectors on the wheel of fortune
-		 * and saves them in the sectors-Array.
-		 * The size of a sector is proportial to its probability, so the following holds:
-		 * sector[i+1]-sectors[i] = (1- (double) (rest-i) / (double) rest)*(2.0/(rest+1)
+		double rest = pop_size - numElite;
+		double[] sectors = new double[(int) rest];
+		int[] indexToSector = new int[(int) rest];
+
+		/*
+		 * Computes the upper bounds of the sectors on the wheel of fortune and
+		 * saves them in the sectors-Array. The size of a sector is proportial
+		 * to its probability, so the following holds: sector[i+1]-sectors[i] =
+		 * (1- (double) (rest-i) / (double) rest)*(2.0/(rest+1)
 		 * 
-		 * Problem: the order of the intervals is always the same!
-		 * Problem: Not sure if its a real distribution
+		 * Problem: the order of the intervals is always the same! Problem: Not
+		 * sure if its a real distribution
 		 */
 		int index = ranking.poll().getValue();
-		elite[index]=false;
+		elite[index] = false;
 		sectors[0] = 0;
-		indexToSector[0]=index;
-		for(int i=1; i<rest;i++){
+		indexToSector[0] = index;
+		for (int i = 1; i < rest; i++) {
 			index = ranking.poll().getValue();
-			elite[index]=false;
-			sectors[i] = sectors[i-1] + 2*(rest-i)/((rest-1)*rest);
-			indexToSector[i-1]=index;
+			elite[index] = false;
+			sectors[i] = sectors[i - 1] + 2 * (rest - i) / ((rest - 1) * rest);
+			indexToSector[i - 1] = index;
 		}
-		
-		for(int i=0;i<sectors.length-1;i++){
-			if(sectors[i]>sectors[i+1]) throw new RuntimeException("Error");
+
+		for (int i = 0; i < sectors.length - 1; i++) {
+			if (sectors[i] > sectors[i + 1])
+				throw new RuntimeException("Error");
 		}
-		
+
 		/*
-		 * Selects the individuals.
-		 * If the individual is an elite(indicated by the elite-Array) it's directly 
-		 * taken in the new population otherwise a wheel of fortune-selection is used
+		 * Selects the individuals. If the individual is an elite(indicated by
+		 * the elite-Array) it's directly taken in the new population otherwise
+		 * a wheel of fortune-selection is used
 		 */
-		for(int i=0;i<pop_size;i++){
-			int tmp = Math.abs(Arrays.binarySearch(sectors,rand.nextDouble()));
-			if(tmp ==rest) tmp--;
-			new_pop [i]=population[elite[i]?i:indexToSector[tmp]];
+		for (int i = 0; i < pop_size; i++) {
+			int tmp = Math.abs(Arrays.binarySearch(sectors, rand.nextDouble()));
+			if (tmp == rest)
+				tmp--;
+			new_pop[i] = population[elite[i] ? i : indexToSector[tmp]];
 		}
-		
+
 		ranking.clear();
 		this.population = new_pop;
-		
+
 	}
-	
-	private void mutateNCross(){
+
+	private void mutateNCross() {
 		Random rand = new Random();
 		double range;
 		/*
-		 * Only non-elite can be mutated. The mutation probability is a class variable.
-		 * The range of the mutation is bounded by twice of the currentvalue of the gene
+		 * Only non-elite can be mutated. The mutation probability is a class
+		 * variable. The range of the mutation is bounded by twice of the
+		 * currentvalue of the gene
 		 */
-		for(int i=0;i<pop_size;i++){
-			//only mutate non-elite
-			if(!elite[i]){
-				for(int j=0;j<genome_length;j++){
-					if(rand.nextDouble()< mutate_probability){
-						range = Math.abs(population[i][j]);
-						population[i][j]= ThreadLocalRandom.current().nextDouble(-range,range+1);
+		for (int i = 0; i < pop_size; i++) {
+			// only mutate non-elite
+			if (!elite[i]) {
+				for (int j = 0; j < genome_length; j++) {
+					if (rand.nextDouble() < mutate_probability) {
+						range = 1.5 * Math.abs(population[i][j]);
+						population[i][j] = ThreadLocalRandom.current().nextDouble(-range, range + 1);
 					}
 				}
-				
+
 			}
 		}
-		
+
 		/*
-		 * Crossovermethod: Shuffle Crossover. The Count of Crossovers is regulated by the class variable
-		 * numCrossover.
+		 * Crossovermethod: Shuffle Crossover. The Count of Crossovers is
+		 * regulated by the class variable numCrossover.
 		 *
-		for(int i=0; i<numCrossover;i++){
-			//select two non-elite
-			
-		}*/
+		 * for(int i=0; i<numCrossover;i++){ //select two non-elite
+		 * 
+		 * }
+		 */
 	}
-	
+
 }
--- Java/NeuralNetwork/src/evolutionaryAlgorithm/Main_Test.java
+++ Java/NeuralNetwork/src/evolutionaryAlgorithm/Main_Test.java
@@ -17,14 +17,14 @@ import basics.PointwiseDistance;
 public class Main_Test {
 
 	public static void main(String[] args) {
-		//graphics
-		XYSeries showFx = new XYSeries("f(x)",true);
-		XYSeries showGx = new XYSeries("g(x)",true);
-		XYSeries showBest = new XYSeries("Beste Approximation",true);
-		
+		// graphics
+		XYSeries showFx = new XYSeries("f(x)", true);
+		XYSeries showGx = new XYSeries("g(x)", true);
+		XYSeries showBest = new XYSeries("Beste Approximation", true);
+
 		XYSeriesCollection collFx = new XYSeriesCollection();
 		collFx.addSeries(showFx);
-		
+
 		XYSeriesCollection collGx = new XYSeriesCollection();
 		collGx.addSeries(showGx);
 		collGx.addSeries(showBest);
@@ -33,64 +33,66 @@ public class Main_Test {
 		NumberAxis y1 = new NumberAxis("Function Output");
 
 		XYLineAndShapeRenderer dotrender = new XYLineAndShapeRenderer();
-		
+
 		XYSplineRenderer spline = new XYSplineRenderer();
 		spline.setPrecision(10);
 		spline.setBaseShapesVisible(false);
-		
+
 		XYPlot plot = new XYPlot();
 		plot.setDataset(0, collFx);
 		plot.setDomainAxis(0, x1);
 		plot.setRangeAxis(0, y1);
 		plot.setRenderer(0, spline);
-		plot.setDataset(1,collGx);
-		plot.setRenderer(1,dotrender);
-		
+		plot.setDataset(1, collGx);
+		plot.setRenderer(1, dotrender);
+
 		JFreeChart chart1 = new JFreeChart(plot);
 		ChartPanel chartPanel1 = new ChartPanel(chart1);
 		ApplicationFrame frame1 = new ApplicationFrame("Keine Ahnung");
 		frame1.setContentPane(chartPanel1);
 		frame1.pack();
-		
-		//generate Data
+
+		// generate Data
 		int datasize = 1001;
 		int approx = 21;
 		int bottom = -10;
 		int ceil = 10;
-		int intervalsize = Math.abs(bottom - ceil); 
-		
-		double [] dataX = new double[1001];
-		double [] dataFx = new double[1001];
-		double [] dataGx = new double[approx];
-		double [] aim = new double[approx];
+		int intervalsize = Math.abs(bottom - ceil);
+
+		double[] dataX = new double[1001];
+		double[] dataFx = new double[1001];
+		double[] dataGx = new double[approx];
+		double[] aim = new double[approx];
 		double x, fx, gx;
-		double err =0;
-		for(int i=0;i<datasize;i++){
-			x= i*(((double ) intervalsize) /((double) datasize-1))- intervalsize / 2;
-			fx = 4*Math.cos(x/5)+Math.sin(1/(2*Math.abs(x)+0.25))+1;
-			if(x-Math.floor(x)==0){
+		double err = 0;
+		for (int i = 0; i < datasize; i++) {
+			x = i * (((double) intervalsize) / ((double) datasize - 1)) - intervalsize / 2;
+			fx = 4 * Math.cos(x / 5) + Math.sin(1 / (2 * Math.abs(x) + 0.25)) + 1;
+			if (x - Math.floor(x) == 0) {
 				gx = ThreadLocalRandom.current().nextDouble();
-				dataGx[(int)x + intervalsize/2]=gx;
-				aim[(int)x + intervalsize/2]=fx;
+				dataGx[(int) x + intervalsize / 2] = gx;
+				aim[(int) x + intervalsize / 2] = fx;
 				showGx.add(x, gx);
-				err +=Math.abs(fx-gx);
+				err += Math.abs(fx - gx);
 			}
-			dataX[i]=x;
-			dataFx[i]=fx;
+			dataX[i] = x;
+			dataFx[i] = fx;
 			showFx.add(x, fx);
 		}
-		err/=1001;
-		System.out.println("Durchschnittlicher Fehler:"+err);
-		//show data
+		err /= 1001;
+		System.out.println("Durchschnittlicher Fehler:" + err);
+		// show data
 		frame1.setVisible(true);
-		
-		
-		Evo strategy = new Evo(approx,100,10,0.02,10, new PointwiseDistance(aim));
+
+		int populationsize=200;
+		double elitefactor= 0.4;
+		double mutation =0.1;
+		Evo strategy = new Evo(approx, populationsize, (int) elitefactor*populationsize, mutation, 10, new PointwiseDistance(aim));
 		strategy.adoptAdaptDevelop(100000);
-		double [] result = strategy.getBest();
-		
-		for(int i=0;i<approx;i++){
-			showBest.add(i-intervalsize/2, result[i]);
+		double[] result = strategy.getBest();
+
+		for (int i = 0; i < approx; i++) {
+			showBest.add(i - intervalsize / 2, result[i]);
 		}
 
 	}
